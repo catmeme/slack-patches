eb6914d1c52ff1ac3b685ebdb79d8717b1cd1c47
Tyler Mulligan <tmulligan@sans.org>
2019-03-26 09:13:31 -0400

    updated dark theme

--- dark-theme.js
+++ dark-theme.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
  $.ajax({
-   // shasum: d3cb311a28c6160787b4eb4e5555433906d1bbfd
-   url: 'https://gist.githubusercontent.com/catmeme/64aab8de30d2a65a4d34ea098fba9be9/raw/2d182d3e033544ed9f148426b48e9fbeae7bc6e8/slack-dark.css',
+   // shasum: f23a8ff0326c496da840563de4c13074e4e7399d
+   url: 'https://raw.githubusercontent.com/catmeme/slack-patches/master/dark-theme.css',
    success: function(css) {
      $("<style></style>").appendTo('head').html(css);
    }
--- dark-theme.sh
+++ dark-theme.sh
@@ -1,6 +1,6 @@
 #/usr/env/bin sh
 
-hash=$(curl https://gist.githubusercontent.com/catmeme/64aab8de30d2a65a4d34ea098fba9be9/raw/2d182d3e033544ed9f148426b48e9fbeae7bc6e8/slack-dark.css 2>/dev/null | shasum |awk '{ print $1 }')
+hash=$(curl https://raw.githubusercontent.com/catmeme/slack-patches/master/dark-theme.css 2>/dev/null | shasum |awk '{ print $1 }')
  
 if grep -q "$hash" dark-theme.js; then
     cat dark-theme.js | sudo tee -a /Applications/Slack.app/Contents/Resources/app.asar.unpacked/src/static/ssb-interop.js
